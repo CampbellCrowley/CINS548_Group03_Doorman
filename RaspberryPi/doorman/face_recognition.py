ae1b757e5dcd1ae56be733ccec59f30b591aca49
Kyle-Crowley <56981757+Kyle-Crowley@users.noreply.github.com>
2020-11-30 12:17:35 -0800

    Update face_recognition.py

--- RaspberryPi/doorman/face_recognition.py
+++ RaspberryPi/doorman/face_recognition.py
@@ -1,9 +1,41 @@
 import face_recognition
+import cv2
+import picamera
+import numpy as np
 
-class FaceRecognition:
-  """
-  Face recognition and identification for Doorman.
-  """
+webcam = cv2.VideoCapture(0)
 
-  def __init__(self):
-    print('Face Recognition!')
+knownFaceEncodings = [
+
+]
+
+knownFaceNames = [
+    "User",
+]
+
+faceLocations = []
+faceEncodings = []
+faceNames = []
+processThisFrame = True
+
+while True:
+    ret, frame = webcam.read()
+    smallFrame = cv2.resize(frame,(0,0),fx=0.25,fy=0.25)
+    rgbSmallFrame = smallFrame[:,:,::-1]
+
+    if processThisFrame:
+        #find all faces in frame and encode them
+        faceLocations = face_recognition.face_locations(rgbSmallFrame)
+        faceEncodings = face_recognition.face_encodings(rgbSmallFrame,faceLocations)
+        faceNames = []
+        #add all faces to known face encoding list
+        knownFaceEncodings.append(faceEncodings)
+
+        processThisFrame = not processThisFrame
+
+    #hit q to quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+VideoCapture.release()
+cv2.destroyAllWindows()
