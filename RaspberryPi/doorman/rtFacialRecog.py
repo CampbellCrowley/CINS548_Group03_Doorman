1616d0a54c7e73b26d514803ae89ba437b8b62e9
Kyle-Crowley <56981757+Kyle-Crowley@users.noreply.github.com>
2020-11-30 12:17:01 -0800

    Update rtFacialRecog.py

--- RaspberryPi/doorman/rtFacialRecog.py
+++ RaspberryPi/doorman/rtFacialRecog.py
@@ -1,7 +1,13 @@
 import face_recognition
 import cv2
+import picamera
+import numpy as np
 
 webcam = cv2.VideoCapture(0)
+
+#load the default user face encoding
+image = face_recognition.load_image_file("User.jpg")
+userEncoding = face_recognition.face_encodings(image)[0]
 
 knownFaceEncodings = [
 
@@ -10,8 +16,6 @@ knownFaceEncodings = [
 knownFaceNames = [
 
 ]
-
-
 
 faceLocations = []
 faceEncodings = []
@@ -30,32 +34,16 @@ while True:
 
         for face_encoding in faceEncodings:
             matches = face_recognition.commpare_faces(knownFaceEncodings,face_encoding)
-            name = "Unknown"
 
             if True in matches:
-                firstMatchIndex = matches.index(true)
-                name = knownFaceNames[firstMatchIndex]
-
-            faceNames.append(name)
+                return True
 
         processThisFrame = not processThisFrame
 
-        for (top,right,bottom,left), name in zip(faceLocations, faceNames):
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
-
-            cv2.rectangle(frame,(left,top), (right,bottom), (0,0,255), 2)
-
-            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-
-        cv2.imshow('Video', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
 VideoCapture.release()
 cv2.destroyAllWindows()
+
